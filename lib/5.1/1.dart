6d8fbdaf5ffb1c119b81f6bc241639ce2bd276a0
MauryaAayush <maayush235@gmail.com>
2024-01-24 16:36:27 +0530

    24/01/2024

---
+++ lib/5.1/1.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // debugShowCheckedModeBanner: ,
+
+      theme: ThemeData(
+
+        // useMaterial3: true,
+      ),
+      home:  SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Flutter App'),
+          ),
+          body: const Center(
+            child: Text('Hello Flutter'),
+          ),
+        ),
+      ),
+
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// Text.rich(
+// TextSpan(
+// text: 'Red & White\n',
+// style: TextStyle(
+// color: Colors.red,
+// fontSize: 65,
+// fontWeight: FontWeight.bold,
+// decoration : TextDecoration.underline,
+// decorationColor: Colors.red,
+// decorationThickness: 1.14,
+// ),
+// children: <TextSpan>[
+// TextSpan(
+// text: '    Multimedia Education\n',
+// style: TextStyle(
+// decoration: TextDecoration.none,
+// fontWeight: FontWeight.bold,
+// fontSize: 30,
+//
+// )
+// )
+// TextSpan()
