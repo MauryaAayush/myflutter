e9f8f10c9ebd4c3a0ec16c418fbd4bbfb3caf619
MauryaAayush <maayush235@gmail.com>
2024-01-25 16:48:46 +0530

    24/01/2024 0.3

--- lib/5.2/Button.dart
+++ lib/5.2/Button.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    statusBarColor: Color(0xFF689237),
+    statusBarColor: Colors.cyan,
   ));
   runApp(const MyApp());
 }
@@ -16,42 +16,48 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
       theme: ThemeData(
 
-        // useMaterial3: true,
-      ),
-      home:  SafeArea(
+          // useMaterial3: true,
+          ),
+      home: SafeArea(
         child: Scaffold(
-            backgroundColor: Color(0xFF689237),
             appBar: AppBar(
-              backgroundColor: Color(0xFF8BC34A),
-
-              title: const Text('My app',style: TextStyle(
-                  color: Colors.white
-              ),),
+              backgroundColor: Colors.blue,
+              title: const Text(
+                'Button',
+                style: TextStyle(color: Colors.white),
+              ),
               centerTitle: true,
-
             ),
             body: Center(
-
-
-            )
-        ),
+              child: Container(
+                alignment: Alignment.center,
+                height: 70,
+                width: 190,
+                decoration: BoxDecoration(
+                  // color: Colors.cyanAccent,
+                  borderRadius: BorderRadius.all(Radius.circular(50)),
+                  gradient: LinearGradient(
+                    colors:[
+                      Colors.purpleAccent,
+                      Colors.pinkAccent,
+                      Colors.redAccent,
+                    ],
+                    begin: Alignment.topLeft,
+                  )
+                ),
+                child: Text(
+                  'Call on action',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            )),
       ),
-
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
