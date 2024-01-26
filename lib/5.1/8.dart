bf37a100223cf132ce375423c9923e6812a6e1bf
MauryaAayush <maayush235@gmail.com>
2024-01-26 23:10:37 +0530

    Radhey Radhey

--- lib/5.1/8.dart
+++ lib/5.1/8.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 void main() {
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
-      statusBarColor: Color(0xFFBBBBBB),
+      statusBarColor: Colors.grey,
     ),
   );
   runApp(const MyApp());
@@ -13,82 +13,85 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-
-          // useMaterial3: true,
-          ),
+      theme: ThemeData(),
       home: SafeArea(
         child: Scaffold(
           backgroundColor: Colors.white,
-          appBar: AppBar(
-            backgroundColor: Color(0xFFBBBBBB),
-            centerTitle: true,
-            title: const Text(''),
-            toolbarHeight: 30,
-          ),
-          body: Center(
-            child: Align(
-              alignment: Alignment.center,
-              child: Stack(
-                children: [
-                  Container(
-                    alignment: Alignment.bottomCenter,
-                    height: 300,
-                    width: 300,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white,
-                      border: Border.all(
-                        color: Color(0xFFFF9800),
-                        width: 40,
-                      ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Expanded(
+                child: Center(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Stack(
+                      children: [
+                        Container(
+                          alignment: Alignment.bottomCenter,
+                          height: 300,
+                          width: 300,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.white,
+                            border: Border.all(
+                              color: Color(0xFFFF9800),
+                              width: 40,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 25,
+                          left: 25.5,
+                          bottom: 60,
+                          child: Container(
+                            height: 230,
+                            width: 250,
+                            decoration: BoxDecoration(
+                              color: Colors.orange,
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 85,
+                          right: 65,
+                          child: Container(
+                            height: 70,
+                            width: 70,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 82,
+                          left: 70,
+                          child: Container(
+                            height: 70,
+                            width: 70,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-
-                  Positioned(
-                    top: 25,
-                    left: 25.5,
-                    bottom: 60,
-                    child: Container(
-                      height: 230,
-                      width: 250,
-                      decoration: BoxDecoration(
-                          color: Colors.orange,
-                          shape: BoxShape.circle),
-                    ),
-                  ),
-                  Positioned(
-                    top: 85,
-                    right: 65,
-                      child: Container(
-                        height: 70,
-                        width: 70,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          shape: BoxShape.circle,
-                        ),
-                      ),
-                  ),
-                  Positioned(
-                    top: 82,
-                    left: 70,
-                      child: Container(
-                        height: 70,
-                        width: 70,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.white,
-                        ),
-                      )
-                  ),
-                ],
+                ),
               ),
-            ),
+              AppBar(
+                backgroundColor: Colors.blue, // Set the background color of the title bar
+                centerTitle: true,
+                title: const Text('Bottom Title'),
+                toolbarHeight: 30,
+              ),
+            ],
           ),
         ),
       ),
