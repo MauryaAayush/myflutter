9ae3163e3487f08face1f7d7b8d1948aa3ac9a2d
MauryaAayush <maayush235@gmail.com>
2024-01-26 19:53:13 +0530

    Radhey Radhey

--- lib/5.1/8.dart
+++ lib/5.1/8.dart
@@ -18,48 +18,51 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
       theme: ThemeData(
 
-        // useMaterial3: true,
-      ),
-      home:  SafeArea(
+          // useMaterial3: true,
+          ),
+      home: SafeArea(
         child: Scaffold(
           backgroundColor: Colors.white,
-            appBar: AppBar(
-              backgroundColor: Color(0xFFBBBBBB),
-              centerTitle: true,
-              title: const Text(''),
-              toolbarHeight: 30,
-            ),
-            body: Center(
-              child: Container(
-                height: 300,
-                width: 300,
-                decoration: BoxDecoration(
-                   shape: BoxShape.circle,
-                  color: Colors.orange,
-                ),
-                child: Container(
-                ),
+          appBar: AppBar(
+            backgroundColor: Color(0xFFBBBBBB),
+            centerTitle: true,
+            title: const Text(''),
+            toolbarHeight: 30,
+          ),
+          body: Center(
+            child: Align(
+              alignment: Alignment.center,
+              child: Stack(
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    height: 300,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                      border: Border.all(
+                        color: Color(0xFFFF9800),
+                        width: 40,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    child: Container(
+                      height: 90,
+                      width: 90,
+                      decoration: BoxDecoration(
+                          color: Colors.blue, shape: BoxShape.circle),
+                    ),
+                  ),
+                ],
               ),
             ),
-
+          ),
         ),
       ),
-
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
