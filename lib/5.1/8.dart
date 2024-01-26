f74bd04df90997e610ce0afa3e0c352e32422a17
MauryaAayush <maayush235@gmail.com>
2024-01-26 23:34:28 +0530

    Radhey Radhey

--- lib/5.1/8.dart
+++ lib/5.1/8.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.grey,
+    ),
+  );
+
   runApp(const MyApp());
 }
 
