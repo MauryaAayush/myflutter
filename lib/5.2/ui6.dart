afb683435b509a7e0bcda64e0d6142d52c71a847
MauryaAayush <maayush235@gmail.com>
2024-01-27 16:05:21 +0530

    Radhey Radhey

--- lib/5.2/ui6.dart
+++ lib/5.2/ui6.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
-
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Color(0xFF1869AB),
+    ),
+  );
   runApp(const MyApp());
 }
 
@@ -13,25 +17,69 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // debugShowCheckedModeBanner: ,
-
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
 
         // useMaterial3: true,
       ),
-      home:  SafeArea(
+      home: SafeArea(
         child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Flutter App'),
-          ),
-          body:  Center(
-            child: Container(
 
+          appBar: AppBar(
+            backgroundColor: const Color(0xFF2196F3),
+            title: const Text(
+              'Dark Shadow Button',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            elevation: 5,
+            shadowColor: Colors.black,
+            centerTitle: true,
+          ),
+          body: Center(
+            child: Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Color(0xFF2294F2),
+                        Color(0xFF2E79D9),
+                        Color(0xFF3764C6),
+                        Color(0xFF3F52B6),
+                      ]
+                  )
+              ),
+              child: Container(
+                  alignment: Alignment.center,
+                  height: 170,
+                  width: 280,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white,width: 1),
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [
+                            Colors.deepOrange,
+                            Colors.deepOrange,
+                            Colors.white,
+                            Colors.green,
+                            Colors.green,
+                          ]
+                      )
+                  ),
+                  child: Text('🛞',
+                      style: TextStyle(
+                        color: Colors.deepPurple,
+                        fontSize: 50,
+                      ))
+              ),
             ),
           ),
         ),
       ),
-
     );
   }
 }
