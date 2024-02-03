aab607ab690c5574a7c7796e25a4772a9a045400
MauryaAayush <maayush235@gmail.com>
2024-02-04 00:53:11 +0530

    Radhey Radhey

---
+++ lib/5.4/Map.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:myflutter/5.4/box.dart';
+
+void main() {
+  runApp(const Map());
+}
+
+class Map extends StatefulWidget {
+  const Map({super.key});
+
+  @override
+  State<Map> createState() => _MapState();
+}
+
+class _MapState extends State<Map> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: Scaffold(
+        backgroundColor: Color(0xFFEEEEEE),
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          toolbarHeight: 60,
+          title: Text(
+            'Map',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          centerTitle: true,
+          leading: Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
+        ),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: List.generate(
+              boxes.length,
+                  (index) => containers(boxes[index]['text'],
+                                        boxes[index]['icons']),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget containers(String text, IconData icons) {
+  return Container(
+    margin: EdgeInsets.symmetric(vertical: 20),
+    height: 90,
+    width: double.infinity,
+    decoration: BoxDecoration(
+      color: Colors.white,
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: 22,
+          ),
+        ),
+        SizedBox.square(
+          dimension: 200,
+        ),
+        Icon(
+          icons
+        ),
+      ],
+    ),
+  );
+}
