6805b8a8f2b6b605a99d99b5878d67abdb2cacb6
MauryaAayush <maayush235@gmail.com>
2024-02-04 20:04:51 +0530

    Radhey Radhey

---
+++ lib/5.4/Icon.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(const Icons());
+}
+
+class Icons extends StatefulWidget {
+  const Icons({super.key});
+
+  @override
+  State<Icons> createState() => _IconsState();
+}
+
+class _IconsState extends State<Icons> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+
+    );
+  }
+}
