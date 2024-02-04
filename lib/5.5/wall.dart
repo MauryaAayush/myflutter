3c5eda75194976e943347e9fd3742da0338c14ac
MauryaAayush <maayush235@gmail.com>
2024-02-04 15:38:27 +0530

    Radhey Radhey

--- lib/5.5/wall.dart
+++ lib/5.5/wall.dart
@@ -125,41 +125,41 @@ class _WallState extends State<Wall> {
                     ),
                   ),
                   Container(
-                    margin: EdgeInsets.symmetric(vertical: 1,horizontal: 10),
+                    margin: const EdgeInsets.symmetric(vertical: 1,horizontal: 10),
                     height: 95,
                     width: 130,
-                    decoration: BoxDecoration(
+                    decoration: const BoxDecoration(
                       color: Color(0xFF5D4037),
                     ),
                   ),
                   Container(
-                    margin: EdgeInsets.symmetric(vertical: 1),
+                    margin: const EdgeInsets.symmetric(vertical: 1),
                     height: 95,
                     width: 131.4,
-                    decoration: BoxDecoration(
+                    decoration: const BoxDecoration(
                       color: Color(0xFF5D4037),
                     ),
                   ),
                 ],),
                 Row(children: [
                   Container(
-                    margin: EdgeInsets.symmetric(vertical: 10),
+                    margin: const EdgeInsets.symmetric(vertical: 10),
                     height: 95,
                     width: 100,
-                    decoration: BoxDecoration(
+                    decoration: const BoxDecoration(
                       color: Color(0xFF5D4037),
                     ),
                   ),
                   Container(
-                    margin: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+                    margin: const EdgeInsets.symmetric(vertical: 10,horizontal: 10),
                     height: 95,
                     width: 191.1,
-                    decoration: BoxDecoration(
+                    decoration: const BoxDecoration(
                       color: Color(0xFF5D4037),
                     ),
                   ),
                   Container(
-                    margin: EdgeInsets.symmetric(vertical: 10),
+                    margin: const EdgeInsets.symmetric(vertical: 10),
                     height: 95,
                     width: 100,
                     decoration: BoxDecoration(
