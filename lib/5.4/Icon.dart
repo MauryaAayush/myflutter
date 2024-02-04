2a5e092860d931bcd22440fe7b4d37732b2ee401
MauryaAayush <maayush235@gmail.com>
2024-02-04 21:34:38 +0530

    Radhey Radhey

--- lib/5.4/Icon.dart
+++ lib/5.4/Icon.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'IconList.dart';
 
 void main()
 {
@@ -18,6 +19,7 @@ class _IconsState extends State<Icons> {
     return  MaterialApp(
         debugShowCheckedModeBanner: true,
       home: Scaffold(
+        backgroundColor: Color(0xFFFEFEFE),
         appBar: AppBar(
           centerTitle: true,
           title: Text('Icons'),
@@ -28,21 +30,84 @@ class _IconsState extends State<Icons> {
 
         body: Column(
           children: [
-            Row(
-              children: [
-                Container(
-                  height: 100,
-                  width: 100,
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                  ),
-                )
-              ],
-            )
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(i1.length, (index) => products(i1[index]['icon']) )
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List.generate(i2.length, (index) => products(i2[index]['icon']) )
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List.generate(i3.length, (index) => products(i3[index]['icon']) )
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List.generate(i4.length, (index) => products(i4[index]['icon']) )
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List.generate(i5.length, (index) => products(i5[index]['icon']) )
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List.generate(i6.length, (index) => products(i6[index]['icon']) )
+                ),
+              ),
+            ),
           ],
         ),
       )
       ,
     );
   }
+}
+
+
+Widget products(IconData icon)
+{
+  return Container(
+    margin: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+    height: 120,
+    width: 120,
+    decoration: BoxDecoration(
+        color: Color(0xFFF5F5F5),
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        boxShadow: [
+          BoxShadow(
+            color: Color(0xFFE3E3E3),
+            offset: Offset(0, 10),
+            blurRadius:  5,
+            spreadRadius: 5,
+          )
+        ]
+    ),
+    child: Icon(icon,size: 50,color: Colors.grey,),
+  );
 }
---
+++ lib/5.4/IconList.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+
+List i1 = [
+  {
+    'icon' : Icons.add
+  },
+  {
+    'icon' : Icons.add_box_outlined
+  },
+  {
+    'icon' : Icons.ac_unit
+  },
+  {
+    'icon' : Icons.access_alarm
+  },
+  {
+    'icon' : Icons.accessibility
+  },
+  {
+    'icon' : Icons.accessible_forward
+  },
+
+];
+
+List i2 = [
+  {
+    'icon' : Icons.account_balance
+  },
+  {
+    'icon' : Icons.account_balance_wallet
+  },
+  {
+    'icon' : Icons.account_circle_outlined
+  },
+  {
+    'icon' : Icons.account_box_outlined
+  },
+  {
+    'icon' : Icons.add_call
+  },
+  {
+    'icon' : Icons.add_circle
+  },
+
+];
+
+List i3 = [
+  {
+    'icon' : Icons.menu
+  },
+  {
+    'icon' : Icons.menu_open
+  },
+  {
+    'icon' : Icons.arrow_circle_right_outlined
+  },
+  {
+    'icon' : Icons.arrow_downward
+  },
+  {
+    'icon' : Icons.arrow_forward
+  },
+  {
+    'icon' : Icons.play_arrow_rounded
+  },
+
+];
+List i4 = [
+  {
+    'icon' : Icons.alarm
+  },
+  {
+    'icon' : Icons.call
+  },
+  {
+    'icon' : Icons.call_end
+  },
+  {
+    'icon' : Icons.call_made_outlined
+  },
+  {
+    'icon' : Icons.call_missed_outgoing
+  },
+  {
+    'icon' : Icons.add_call
+  },
+
+];
+
+List i5 = [
+  {
+    'icon' : Icons.search
+  },
+  {
+    'icon' : Icons.find_in_page
+  },
+  {
+    'icon' : Icons.wallet_giftcard_sharp
+  },
+  {
+    'icon' : Icons.safety_check
+  },
+  {
+    'icon' : Icons.security
+  },
+  {
+    'icon' : Icons.qr_code
+  },
+
+];
+
+List i6 = [
+  {
+    'icon' : Icons.opacity
+  },
+  {
+    'icon' : Icons.bolt
+  },
+  {
+    'icon' : Icons.apps
+  },
+  {
+    'icon' : Icons.settings_accessibility
+  },
+  {
+    'icon' : Icons.install_mobile
+  },
+  {
+    'icon' : Icons.view_cozy
+  },
+
+];
