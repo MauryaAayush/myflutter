bc5c3a600d29550107873a5bc125cef1de14968d
MauryaAayush <maayush235@gmail.com>
2024-02-04 23:46:11 +0530

    Radhey Radhey

---
+++ lib/5.4/IconsEditor.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(const Selector());
+}
+
+class Selector extends StatefulWidget {
+  const Selector({super.key});
+
+  @override
+  State<Selector> createState() => _SelectorState();
+}
+
+class _SelectorState extends State<Selector> {
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xFFEFF1F3),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+        ),
+        body: Container(),
+      ),
+    );
+  }
+}
