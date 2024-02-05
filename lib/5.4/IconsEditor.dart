363c06133a806482e057fccec4710844f550fde5
MauryaAayush <maayush235@gmail.com>
2024-02-05 07:41:24 +0530

    Radhey Radhey

--- lib/5.4/IconsEditor.dart
+++ lib/5.4/IconsEditor.dart
@@ -1,21 +1,39 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
+void main() {
   runApp(const Selector());
 }
 
 class Selector extends StatefulWidget {
-  const Selector({super.key});
+  const Selector({Key? key}) : super(key: key);
 
   @override
-  State<Selector> createState() => _SelectorState();
+  _SelectorState createState() => _SelectorState();
 }
 
 class _SelectorState extends State<Selector> {
+  List<Color> colors = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.yellow,
+    Colors.purple,
+  ];
+
+  List<IconData> icons = [
+    Icons.android,
+    Icons.add_call,
+    Icons.add_circle,
+    Icons.account_balance,
+    Icons.add_box,
+  ];
+
+  int selectedColorIndex = 0;
+  int selectedIconIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Color(0xFFEFF1F3),
@@ -23,8 +41,88 @@ class _SelectorState extends State<Selector> {
           backgroundColor: Colors.white,
           title: Text('Icons Editor'),
           centerTitle: true,
+          elevation: 5,
+          shadowColor: Colors.black,
         ),
-        body: Container(),
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              child: Icon(
+                icons[selectedIconIndex],
+                size: 60,
+                color: colors[selectedColorIndex],
+              ),
+            ),
+
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20),
+              height: 100,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: icons.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedIconIndex = index;
+                      });
+                    },
+                    child: Container(
+                      width: 80,
+                      height: 80,
+                      margin: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16),
+                        border: selectedIconIndex == index
+                            ? Border.all(color: Colors.black, width: 2)
+                            : null,
+                      ),
+                      child: Icon(
+                        icons[index],
+                        size: 40,
+                        color: colors[selectedColorIndex],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20),
+              height: 100,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: colors.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        selectedColorIndex = index;
+                      });
+                    },
+                    child: Container(
+                      width: 80,
+                      height: 80,
+                      margin: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: colors[index],
+                        borderRadius: BorderRadius.circular(16),
+                        border: selectedColorIndex == index
+                            ? Border.all(color: Colors.black, width: 2)
+                            : null,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            // Display the selected icon at the center
+
+          ],
+        ),
       ),
     );
   }
---
+++ lib/5.4/SelectList.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+List colors = [
+  {'color': Colors.blue},
+  {'color': Colors.green},
+  {'color': Colors.yellow},
+  {'color': Colors.purple},
+  {'color': Colors.orange},
+];
+
+List icons = [
+  {'icon': Icons.android},
+  {'icon': Icons.apple},
+  {'icon': Icons.sports_soccer},
+  {'icon': Icons.music_note},
+  {'icon': Icons.airplanemode_active},
+  {'icon': Icons.directions_car},
+];
