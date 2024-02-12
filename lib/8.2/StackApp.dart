75853e32a981bee681b4e01c7908a9669ce761e6
MauryaAayush <maayush235@gmail.com>
2024-02-12 11:31:46 +0530

    Radhey Radhey

--- lib/8.2/StackApp.dart
+++ lib/8.2/StackApp.dart
@@ -206,7 +206,7 @@ class _StackappState extends State<Stackapp> {
                   padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
                   height: 150,
                   width: 150,
-                  decoration: BoxDecoration(
+                  decoration: const BoxDecoration(
                       color: Colors.redAccent,
                       borderRadius: BorderRadius.all(Radius.circular(20)),
                       boxShadow: [
