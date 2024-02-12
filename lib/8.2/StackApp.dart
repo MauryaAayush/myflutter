5557bfcac55c033a1359a64eafd771f6512c202e
MauryaAayush <maayush235@gmail.com>
2024-02-12 11:12:36 +0530

    Radhey Radhey

---
+++ lib/8.2/StackApp.dart
@@ -0,0 +1,234 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main()
+{
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.grey,
+        ));
+  runApp(const Stackapp());
+}
+
+class Stackapp extends StatefulWidget {
+  const Stackapp({super.key});
+
+  @override
+  State<Stackapp> createState() => _StackappState();
+}
+
+class _StackappState extends State<Stackapp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+           backgroundColor: Colors.white,
+            title: Text('Stack App',style: TextStyle(
+              color: Colors.black,
+              fontSize: 26,
+              fontWeight: FontWeight.w600,
+            ),
+            ),
+            centerTitle: true,
+            toolbarHeight: 60,
+          ),
+          body: Stack(
+            children: [
+              Positioned(
+                top: 20,
+                left: 20,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                  height: 150,
+                  width: 150,
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 10,
+                        spreadRadius: 2,
+                        offset: Offset(5, 10),
+                      )
+                    ]
+                  ),
+                  child: Text('Purple',style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                  ),),
+                ),
+              ),
+
+              Positioned(
+                top: 60,
+                left: 55,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                  height: 150,
+                  width: 150,
+                  decoration: BoxDecoration(
+                      color: Colors.indigo,
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 10,
+                          spreadRadius: 2,
+                          offset: Offset(5, 10),
+                        )
+                      ]
+                  ),
+                  child: Text('Indigo',style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                  ),),
+                ),
+              ),
+
+              Positioned(
+                top: 100,
+                left: 95,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                  height: 150,
+                  width: 150,
+                  decoration: BoxDecoration(
+                      color: Colors.lightBlue,
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 10,
+                          spreadRadius: 2,
+                          offset: Offset(5, 10),
+                        )
+                      ]
+                  ),
+                  child: Text('LightBlue',style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                  ),),
+                ),
+              ),
+
+              Positioned(
+                top: 145,
+                left: 140,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                  height: 150,
+                  width: 150,
+                  decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 10,
+                          spreadRadius: 2,
+                          offset: Offset(5, 10),
+                        )
+                      ]
+                  ),
+                  child: Text('Green',style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                  ),),
+                ),
+              ),
+
+              Positioned(
+                top: 188,
+                right: 85,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                  height: 150,
+                  width: 150,
+                  decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 10,
+                          spreadRadius: 2,
+                          offset: Offset(5, 10),
+                        )
+                      ]
+                  ),
+                  child: Text('Amber',style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                  ),),
+                ),
+              ),
+
+              Positioned(
+                top: 235,
+                right: 60,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                  height: 150,
+                  width: 150,
+                  decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 10,
+                          spreadRadius: 2,
+                          offset: Offset(5, 10),
+                        )
+                      ]
+                  ),
+                  child: Text('Orange',style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                  ),),
+                ),
+              ),
+
+              Positioned(
+                bottom: 20,
+                right: 20,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                  height: 150,
+                  width: 150,
+                  decoration: BoxDecoration(
+                      color: Colors.redAccent,
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          blurRadius: 10,
+                          spreadRadius: 2,
+                          offset: Offset(5, 10),
+                        )
+                      ]
+                  ),
+                  child: Text('RedAccent',style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w600,
+                  ),),
+                ),
+              ),
+
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
